bb104c92db4a473cb09b20c4fea15e2663bc9a03
Ads.com <samiseyam56@gmail.com>
2026-07-05 11:47:50 +0500

    Create cw.js

---
+++ cw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Free pass for all requests
+  e.respondWith(fetch(e.request));
+});
