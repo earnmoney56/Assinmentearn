ef487bcd1b3850cc8704e957d6bafa9d772dcbd0
Ads.com <samiseyam56@gmail.com>
2026-07-16 14:26:42 +0500

    Refactor service worker fetch event handling

--- sw.js
+++ sw.js
@@ -1,24 +1,8 @@
-const CACHE_NAME = 'chat-v1';
-const ASSETS = [
-  './index.html',
-  './manifest.json'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  // Simple fetch handler to satisfy PWA requirements
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
